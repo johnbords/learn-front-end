0a7d1e5e4e8daf9c4b6aebc9fb5ba30b233b3e7d
Biboy <johnnephiaquindeb@gmail.com>
2026-05-09 19:29:00 -1000

    Add navigation links to Header component

--- fakebook-frontend/src/Header.jsx
+++ fakebook-frontend/src/Header.jsx
@@ -3,6 +3,15 @@ function Header() {
     return (
         <header>
             <h1>Fakebook</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/profile">Profile</a></li>
+                    <li><a href="/messages">Messages</a></li>
+                    <li><a href="/notifications">Notifications</a></li>
+                </ul>
+            </nav>
+            <hr />
         </header>
     );
 }
