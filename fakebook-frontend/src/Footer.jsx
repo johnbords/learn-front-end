ac9ace226091aff2c5e5780fbb3d30fdaedca62e
Biboy <johnnephiaquindeb@gmail.com>
2026-05-09 19:33:51 -1000

    Add Footer component

---
+++ fakebook-frontend/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Fakebook. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
