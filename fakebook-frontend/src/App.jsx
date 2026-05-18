98c741e4105b84e3305a0559de148fbf16b13d8e
Biboy <johnnephiaquindeb@gmail.com>
2026-05-18 13:32:23 -1000

    Refactor App component to use posts array for testing PostCard component

--- fakebook-frontend/src/App.jsx
+++ fakebook-frontend/src/App.jsx
@@ -2,13 +2,22 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import PostCard from './PostCard.jsx';
 
+{/* Lists and Keys for sample posts - DELETE THIS LATER */}
+const posts = [
+  { id: 1, name: "John Nephi", content: "My first post!" },
+  { id: 2, name: "Revie", content: "React is fun." },
+];
+
 function App() {
   return(
     <>
       <Header />
-      
-      <PostCard name = "John Nephi" content = "sample Post card" />
 
+      {/* Sample Posts - DELETE THIS LATER */}
+      {posts.map(post => (
+        <PostCard key={post.id} name={post.name} content={post.content} />
+      ))}
+      
       <Footer />
     </>
   );
