5caefd861a333381571d267c6a06679b940482a4
Biboy <johnnephiaquindeb@gmail.com>
2026-05-27 21:30:57 -1000

    Week 3: Add Signup page

--- fakebook-frontend/src/App.jsx
+++ fakebook-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Home from './Home.jsx';
 import Login from './Login.jsx';
+import Signup from './Signup.jsx';
 
 function App() {
   return(
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       
       <Footer />
---
+++ fakebook-frontend/src/Signup.jsx
@@ -0,0 +1,10 @@
+
+function Signup()  {
+    return(
+        <div className="pt-5 mt-5 text-center">
+            <h1>Signup</h1>
+        </div>
+    );
+}
+
+export default Signup;
