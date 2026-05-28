cad85ba1067f826312955dbdb441a89d3a56c665
Biboy <johnnephiaquindeb@gmail.com>
2026-05-27 21:48:49 -1000

    Week 3: Add Profile page

--- fakebook-frontend/src/App.jsx
+++ fakebook-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from './Footer.jsx';
 import Home from './Home.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
+import Profile from './Profile.jsx';
 
 function App() {
   return(
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       
       <Footer />
