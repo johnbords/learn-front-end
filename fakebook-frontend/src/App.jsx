219a6577f0763c42bd28a8d203db321c6f262a6e
Biboy <johnnephiaquindeb@gmail.com>
2026-05-27 21:26:43 -1000

    Week 3: Add Login page

--- fakebook-frontend/src/App.jsx
+++ fakebook-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Home from './Home.jsx';
+import Login from './Login.jsx';
 
 function App() {
   return(
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
       
       <Footer />
---
+++ fakebook-frontend/src/Login.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function Login() {
+    return (
+        <div className="pt-5 mt-5 text-center">
+            <h1>Login</h1>
+        </div>
+    );
+}
+
+export default Login;
