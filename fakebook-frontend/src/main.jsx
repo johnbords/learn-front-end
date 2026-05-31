ec2f5151ac1b7fad9524c4942034ffc4128b8244
Biboy <johnnephiaquindeb@gmail.com>
2026-05-30 19:14:57 -1000

    Week 4: Add blue-purple gradient color for background

--- fakebook-frontend/src/main.jsx
+++ fakebook-frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css'
 import './PostCard.css'
 import App from './App.jsx'
-import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
