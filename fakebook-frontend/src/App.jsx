0cb4918b4a802ed2ecc916e01322e001e7471186
Biboy <johnnephiaquindeb@gmail.com>
2026-06-09 11:59:53 -1000

    Add ProtectedRoute component and update routing in App to enforce authentication

--- fakebook-frontend/src/App.jsx
+++ fakebook-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './Login.jsx';
 import Signup from './Signup.jsx';
 import Profile from './Profile.jsx';
 import PostDetails from "./pages/PostDetails";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return(
@@ -17,8 +18,34 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/posts/:id" element={<PostDetails />} />
+          
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/posts/:id"
+            element={
+              <ProtectedRoute>
+                <PostDetails />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
         </Routes>
         
         <Footer />
--- fakebook-frontend/src/api/postsApi.js
+++ fakebook-frontend/src/api/postsApi.js
@@ -3,17 +3,30 @@ const API_URL_BACKEND = "http://localhost:3000";
 export async function getPosts() {
   const token = localStorage.getItem("token");
 
-  const response = await fetch(
-    "http://localhost:3000/posts",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
+  const response = await fetch(`${API_URL_BACKEND}/posts`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
   if (!response.ok) {
     throw new Error("Failed to fetch posts");
+  }
+
+  return await response.json();
+}
+
+export async function getPostById(id) {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch(`${API_URL_BACKEND}/posts/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch post");
   }
 
   return await response.json();
